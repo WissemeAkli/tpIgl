780a95f438ec34472c354d4cd127645ed0b82057
akram09 <a.boutouchent@esi-sba.dz>
2019-12-31 18:17:34 +0100

    finished adding the rest api

---
+++ database/migrations/2019_12_31_165349_create_module_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateModuleTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('module', function (Blueprint $table) {
+              $table->bigIncrements('id');
+              $table->string("name");
+              $table->integer("teacher_id");
+              $table->integer("year");
+                        $table->integer("semestre");
+                        $table->integer("groupe_id");
+                        $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('module');
+    }
+}
---
+++ database/migrations/2019_12_31_165513_create_note_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateNoteTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('note', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer("module_id");
+            $table->integer("etudiant_id");
+            $table->enum("type", ["Td", "TF", "CF"]);
+            $table->float("valeur");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('note');
+    }
+}
