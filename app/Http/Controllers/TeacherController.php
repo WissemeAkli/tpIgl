c83ce356f31f7a911440228e46188ccbec84811f
akram09 <a.boutouchent@esi-sba.dz>
2020-01-01 04:15:31 +0100

    finished getting all the groups a teacher teachs

--- app/Http/Controllers/TeacherController.php
+++ app/Http/Controllers/TeacherController.php
@@ -3,6 +3,12 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\Controller;
+use App\Teacher;
+use App\Groupe;
+use Log ;
+use App\Module;
 
 class TeacherController extends Controller
 {
@@ -12,9 +18,17 @@ class TeacherController extends Controller
             return response()->json(['error'=>"You are A student you can't access teacher space"], 401);
         }
         $teacherModules = Module::where("teacher_id" , $user->idCompte)->get();
-        print_r($teacherModules)
-
-
-
+        $modules= [];
+        foreach($teacherModules as $module){
+            $groupe_id = $module->groupe_id;
+            $groupeName = Groupe::where("id", $groupe_id)->first()->name;
+            $moduleName = $module->name;
+            if(array_key_exists( $moduleName, $modules)){
+                $modules[$moduleName][$groupe_id]= $groupeName;
+            }else{
+                $modules[$moduleName] = [$groupe_id => $groupeName];
+            }
+        }
+        return response()->json(["succes"=>$modules, "user"=>$user]);
     }
 }
