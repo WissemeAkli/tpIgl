39cb410ad3b8b37eebeeff0fdb16bf221f487c38
akram09 <a.boutouchent@esi-sba.dz>
2020-01-02 19:02:10 +0100

    fix cors error

--- routes/api.php
+++ routes/api.php
@@ -12,9 +12,11 @@ use Illuminate\Http\Request;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
-Route::post('login', 'UserController@login');
-Route::post('register', 'UserController@register');Route::group(['middleware' => 'auth:api'], function(){
+Route::group(['middleware' =>  'cors'], function(){
+    Route::post('login', 'UserController@login');
+    Route::post('register', 'UserController@register');
+});
+Route::group(['middleware' => 'auth:api', 'cors'], function(){
     Route::get('teacher', 'TeacherController@details');
     Route::post('teacher/groupe', 'TeacherController@groupe');
     Route::post('teacher/groupe/addNote', 'TeacherController@addNote');
